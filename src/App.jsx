47b8b3301a1b5c41e05cc39232cb367d62de8caf
jiialex <jiialex11@gmail.com>
2025-07-30 03:33:59 +0300

    Initial project setup and basic React structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './Components/Navbar';
+function App() {
+  return (
+    <>
+     <BrowserRouter>
+     <Routes>
+<Route path="" element={<Navbar/>}/>
+     </Routes>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
